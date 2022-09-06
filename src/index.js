04d91fa09dfc7d180a5c5ef2470460b987e0eb10
TemesgeA <temesgen.ayneka@gmail.com>
2022-09-06 18:15:30 +0300

    add modal button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import _ from 'lodash';
+import getResponse from './modules/apiHandler.js';
+console.log(getResponse);
+getResponse();
+
+// create new elments
+var newHeading = document.createElement('h1');
+var newParagraph = document.createElement('p');
+
+// create text nodes for new elements
+var h1Text= document.createTextNode('This is a nice header text!');
+var pText= document.createTextNode('This is a nice paragraph text!');
+
+// attach new text nodes to new elements
+newHeading.appendChild(h1Text);
+newParagraph.appendChild(pText);
+
+// elements are now created and ready to be added to the DOM.
