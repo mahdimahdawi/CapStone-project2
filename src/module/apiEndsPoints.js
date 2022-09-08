9d83564658218b3dc443f471568e64b3b20e52e4
TemesgeA <temesgen.ayneka@gmail.com>
2022-09-08 21:16:43 +0300

    create api endpoints

---
+++ src/module/apiEndsPoints.js
@@ -0,0 +1,2 @@
+export const SHOW_BASE_URL = 'https://api.tvmaze.com';
+export const INVOLVEMENT_API_BASE_URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Imf4csndvBnR19rGhvHc';
