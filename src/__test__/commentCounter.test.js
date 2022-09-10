721b96b8e725da5e0aea225f43ca6ede1780b5cf
TemesgeA <temesgen.ayneka@gmail.com>
2022-09-10 10:36:12 +0300

    test code

--- src/__test__/commentCounter.test.js
+++ src/__test__/commentCounter.test.js
@@ -3,11 +3,11 @@ import CommentsPopup from '../module/popupComments.js';
 test('Count the array of comments and render the number', () => {
   const comment = new CommentsPopup();
   const arr = [
-    { comment: 1, creation_date: '2021-01-10', username: 'name1' },
-    { comment: 1, creation_date: '2021-01-09', username: 'name2' },
-    { comment: 1, creation_date: '2021-01-10', username: 'name3' },
-    { comment: 1, creation_date: '2021-01-11', username: 'name4' },
-    { comment: 1, creation_date: '2021-01-10', username: 'name5' },
+    { comment: 1, creation_date: '2021-01-10', username: 'beky' },
+    { comment: 1, creation_date: '2021-01-09', username: 'james' },
+    { comment: 1, creation_date: '2021-01-10', username: 'nady' },
+    { comment: 1, creation_date: '2021-01-11', username: 'brad' },
+    { comment: 1, creation_date: '2021-01-10', username: 'mafi' },
   ];
 
   const length = comment.countComments(arr);
